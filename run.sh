40d71e60c8a51691996077288ed44b97f8935d87
CNLHC <cn_lhc@qq.com>
2019-11-21 00:43:47 +0800

    a more comprehensive entry class and auto runner script

---
+++ run.sh
@@ -0,0 +1,15 @@
+if [ "$1" = "hadoop" ]
+then
+        echo using hadoop
+        hadoop jar /artifact/DataLab-assembly-0.1.0-SNAPSHOT.jar $1 $2
+elif [ "$1" = "spark" ]
+then
+        echo using spark
+        spark-submit /artifact/DataLab-assembly-0.1.0-SNAPSHOT.jar $1 $2
+else
+        echo "wrong method, use hadoop or spark"
+        exit -1
+fi
+
+hadoop fs -getmerge BUAA/problem$2/ou result-$2
+hadoop fs -rm -r BUAA/problem$2/ou
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,27 +7,50 @@ import solution.problem5.HadoopMain.{run => P5Hadoop}
 import solution.problem6.HadoopMain.{run => P6Hadoop}
 import solution.problem0.SparkMain.{run => P0Spark}
 import solution.problem1.SparkMain.{run => P1Spark}
-import org.apache.hadoop.fs.FileSystem
-import org.apache.hadoop.conf.Configuration
-
-
-
+import solution.problem2.SparkMain.{run => P2Spark}
+import solution.problem3.SparkMain.{run => P3Spark}
+import solution.problem4.SparkMain.{run => P4Spark}
+import solution.problem5.SparkMain.{run => P5Spark}
+import solution.problem6.SparkMain.{run => P6Spark}
 
 
 object Main {
-  val  namenodeAddres= "hdfs://localhost:9000"
+  val namenodeAddres = "hdfs://localhost:9000"
+
+  def hadoopInput = (no: Int) => s"$namenodeAddres/user/root/BUAA/problem$no/in/"
+
+  def sparkInput = (no: Int) => s"$namenodeAddres/user/root/BUAA/problem$no/in/input"
+
+  def output = (no: Int) => s"$namenodeAddres/user/root/BUAA/problem$no/ou/"
+
+
   def main(args: Array[String]): Unit = {
-    println("Welcome to DataLab P1")
-
-//    P0Hadoop("in", "ou")
-//    P1Hadoop("in", "ou")
-//    P2Hadoop("in", "ou")
-//    P3Hadoop("in", "ou")
-//    P4Hadoop("in", "ou")
-//    P5Hadoop("in", "ou")
-//    P6Hadoop("in", "ou")
-//    P0Spark("hdfs://localhost:9000/user/root/BUAA/problem0/in/input","hdfs://localhost:9000/user/root/BUAA/problem0/ou/")
-      P1Spark("/user/root/BUAA/problem1/in/input","hdfs://localhost:9000/user/root/BUAA/problem1/ou/")
-
+    val s = args.mkString(",")
+    println(s)
+    args(0) match {
+      case "hadoop" =>
+        args(1) match {
+          case "0" => P0Hadoop(hadoopInput(0), output(0))
+          case "1" => P1Hadoop(hadoopInput(1), output(1))
+          case "2" => P2Hadoop(hadoopInput(2), output(2))
+          case "3" => P3Hadoop(hadoopInput(3), output(3))
+          case "4" => P4Hadoop(hadoopInput(4), output(4))
+          case "5" => P5Hadoop(hadoopInput(5), output(5))
+          case "6" => P6Hadoop(hadoopInput(6), output(6))
+          case _ => println("no problem find")
+        }
+      case "spark" =>
+        args(1) match {
+          case "0" => P0Spark(sparkInput(0), output(0))
+          case "1" => P1Spark(sparkInput(1), output(1))
+          case "2" => P2Spark(sparkInput(2), output(2))
+          case "3" => P3Spark(sparkInput(3), output(3))
+          case "4" => P4Spark(sparkInput(4), output(4))
+          case "5" => P5Spark(sparkInput(5), output(5))
+          case "6" => P6Spark(sparkInput(6), output(6))
+          case _ => println("no problem find")
+        }
+      case _ => println("usage: <hadoop>|<spark> ")
+    }
   }
 }
