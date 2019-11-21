4ede1e0a5e0d44e2b59e084fe7d900e835ffc595
CNLHC <cn_lhc@qq.com>
2019-11-21 13:57:06 +0800

    solve matrix multiply problem using hadoop

--- src/main/scala/solution/problem6/MatrixMultiplyMapper.scala
+++ src/main/scala/solution/problem6/MatrixMultiplyMapper.scala
@@ -3,24 +3,24 @@ package solution.problem6
 import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
 import org.apache.hadoop.mapreduce.Mapper
 import io.circe.parser.parse
+import io.circe.syntax._
 
 class MatrixMultiplyMapper extends Mapper[LongWritable, Text, Text, Text] {
   val MATRIX_SIZE = 10
 
   override
   def map(key: LongWritable, value: Text, context: Mapper[LongWritable, Text, Text, Text]#Context): Unit = {
-    parse(value.toString) match {
-      case Left(_) => ()
-      case Right(json) => json.asArray match {
-        case None => ()
-        case Some(jsonArr) => jsonArr.head.asString.getOrElse("") match {
-          case "a" => (0 to 9).foreach(i =>
-            context.write(new Text((i, jsonArr(1).asNumber.getOrElse(-1)).toString), new Text(value.toString)))
-          case "b" => (0 to 9).foreach(j =>
-            context.write(new Text((jsonArr(1).asNumber.getOrElse(-1), j).toString), new Text(value.toString)))
-          case _ => ()
-        }
-      }
+
+    lazy val unit = ("", 0, 0, 0)
+    val data = parse(value.toString).getOrElse(unit.asJson).as[Tuple4[String, Int, Int, Int]].getOrElse(unit)
+
+    data._1 match {
+      case "a" => (0 to 9).foreach(i =>
+        context.write(new Text((data._2, i).toString), new Text(value.toString)))
+      case "b" => (0 to 9).foreach(j =>
+        context.write(new Text((j, data._3).toString), new Text(value.toString)))
+      case _ => ()
     }
+
   }
 }
--- src/main/scala/solution/problem6/MatrixMultiplyReducer.scala
+++ src/main/scala/solution/problem6/MatrixMultiplyReducer.scala
@@ -4,6 +4,8 @@ import org.apache.hadoop.io.{NullWritable, Text}
 import org.apache.hadoop.mapreduce.Reducer
 import io.circe.parser.parse
 import io.circe.syntax._
+import org.apache.hadoop.io
+
 import scala.collection.JavaConverters._
 
 
@@ -13,39 +15,32 @@ class MatrixMultiplyReducer extends Reducer[Text, Text, NullWritable, Text] {
 
     //    context.write(NullWritable.get(), new Text(values.asScala.foldLeft("")((a, c) => a + new Text(c).toString)))
     //  }
-    val idx = key.toString.replaceAll("\\(|\\)", "").split(",").map(_.toInt)
-    val data: Map[String, Iterable[(String, Int, Int, Int)]] = values.asScala.map(e => parse(new Text(e).toString) match {
-      case Left(_) => ("", -1, -1, -1)
-      case Right(json) => json.asArray match {
-        case None => ("", -1, -1, -1)
-        case Some(jsonArr) =>
-          val a = ((
-            jsonArr(0).asString.getOrElse(""),
-            jsonArr(1).asNumber.getOrElse(1),
-            jsonArr(2).asNumber.getOrElse(1),
-            jsonArr(3).asNumber.getOrElse(1),
-          ))
-          return a
-      }
-    }).filter(t => t._1 == "a" || t._1 == "b")
-      .groupBy(_._1)
+    //    val idx = key.toString.replaceAll("\\(|\\)", "").split(",").map(_.toInt)
+    lazy val unit = ("", 0, 0, 0)
+
+    val data: Map[String, Iterable[(String, Int, Int, Int)]] = values.asScala.map(e => {
+      val value = new io.Text(e).toString
+      val data = parse(value.toString).getOrElse(unit.asJson).as[Tuple4[String, Int, Int, Int]].getOrElse(unit)
+      (data._1, data)
+    }).groupBy(_._1).mapValues(_.map(_._2))
 
 
     val aRow = data.get("a")
     val bRow = data.get("b")
-//    context.write(NullWritable.get(), new Text(aRow.foldLeft("")((a, c) => a + new Text(c.toString()).toString)))
-      var result = 0
-      (aRow, bRow) match {
-        case (Some(a), Some(b)) => a.foreach(e => b.foreach(v => {
-          if (e._3 == v._2)
-            result += e._4 * v._4
-        }))
-        case _ => ()
-      }
 
+    var result = 0
+    (aRow, bRow) match {
+      case (Some(a), Some(b)) => a.foreach(e => b.foreach(v => {
+        if (e._3 == v._2)
+          result += e._4 * v._4
+      }))
+      case _ => ()
+    }
+
+    if (result != 0)
       context.write(NullWritable.get(), new Text(
-        List(idx.head, idx.last, result).asJson.noSpaces
+        (key.toString, result).asJson.noSpaces
       ))
 
-    }
+  }
 }
